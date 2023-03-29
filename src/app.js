412b57011b9d55e67e0105f0f8bbb29e4d8b8a84
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-03-29 23:41:16 +0100

    adding api documentation

--- src/app.js
+++ src/app.js
@@ -7,6 +7,36 @@ const userRouter = require("./routes/userRoutes");
 const eventRouter = require("./routes/eventRoutes");
 const authRoutes = require("./routes/authRoutes");
 
+function index(req, res) {
+  let data = {
+    message: 'Welcome to the eco.io api, check out the routes below:',
+    routes: {
+      "/": 'you are here',
+      "/auth": [
+        ["POST /auth/register", "create account include these values { username, email, password }"],
+        ["POST /auth/login ", "login to account include these values { username, password }"],
+        ["GET /auth/logout", "log out of an account and clear user session data"]
+      ],
+      "/events": [
+        ["GET events/all", "get all unapproved events"],
+        ["GET events/a/all", "get all approved events"],
+        ["PATCH events/v/:event_id", "upvote or downvote an event"],
+        ["GET events/", "get an account's events"],
+        ["POST events/", "create an event"],
+        ["DELETE events/:event_id", "delete an existing event"],
+      ],
+      "/users": [
+        ["GET users/top", "get the top 10 users ordered by events attended"],
+        ["GET users/bookings", "get all the users bookings depending on who's signed in"],
+        ["POST users/bookings/", "create a booking { user_id, event_id } - needs improving"],
+        ["GET users/h/bookings", "get all the users attended bookings depending on who's signed in"],
+      ]
+    }
+  }
+
+  return res.status(200).json(data)
+}
+
 const app = express();
 
 // middleware
@@ -23,22 +53,7 @@ app.use(
 );
 
 // routes
-app.get("/", (req, res) => {
-  res.json({
-    todo: [
-      "leaderboard functionality",
-    ],
-    done: [
-      "allow storage, creating, updating and deleting of events",
-      "allow storage, creating of users",
-      "create db",
-      "build auth",
-      "hash passwords",
-      "get sessions working",
-    ],
-  });
-});
-
+app.use('/', index)
 app.use("/auth", authRoutes);
 app.use("/events", eventRouter);
 app.use('/users', userRouter);
