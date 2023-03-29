5cb747ea4f9e97b61acaece20a4edbfe2140f3da
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-03-29 10:01:46 +0100

    adding comments to auth routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,8 +4,13 @@ const userController = require('../controllers/userController.js');
 
 const userRouter = Router();
 
+// POST /auth/register - create account include these values { username, email, password, isAdmin }
 userRouter.post("/register", userController.register);
+
+// POST /auth/login - login to account include these values { username, password }
 userRouter.post("/login", userController.login);
+
+// GET /auth/logout - log out of an account and clear user session data
 userRouter.get("/logout", userController.logout);
 
 module.exports = userRouter;
